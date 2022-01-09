b9c57067b758056ea7730f32a73354fc4f262f31
Tofique Ahmed Khan <tofiquekhan171819@gmail.com>
2022-01-09 22:08:01 +0530

    Constructor Dependence Injection Example

---
+++ src/myproject/constructordepeninjection/beans/Course.java
@@ -0,0 +1,28 @@
+package myproject.constructordepeninjection.beans;
+
+public class Course {
+
+	private String cid;
+	private String cname;
+	private int cost;
+	
+	
+	
+	public Course(String cid, String cname, int cost) {
+		super();
+		this.cid = cid;
+		this.cname = cname;
+		this.cost = cost;
+	}
+
+	public void getCourseDetails() {
+		System.out.println("Course Details");
+		System.out.println("-------------------------------");
+		System.out.println("Course Id 		: "+cid);
+		System.out.println("Course Name		: "+cname);
+		System.out.println("Course Cost		: "+cost);
+	}
+	
+	
+	
+}
---
+++ src/myproject/constructordepeninjection/test/Test.java
@@ -0,0 +1,15 @@
+package myproject.constructordepeninjection.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import myproject.constructordepeninjection.beans.Course;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("/myproject/constructordepeninjection/resources/applicationContext.xml");
+		Course courseBean = (Course) context.getBean("courseBean");
+		courseBean.getCourseDetails();
+	}
+}
