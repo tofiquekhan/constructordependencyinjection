cd6fe6c8bbf1dd78d0dae7f77da067b669cb5a9f
Tofique Ahmed Khan <tofiquekhan171819@gmail.com>
2022-01-09 22:29:07 +0530

    Constructor Overloading and calling

--- src/myproject/constructordepeninjection/beans/Course.java
+++ src/myproject/constructordepeninjection/beans/Course.java
@@ -6,10 +6,28 @@ public class Course {
 	private String cname;
 	private int cost;
 	
+	public Course() {
+		super();
+		System.out.println("0-args Constructor");
+	}
 	
+	public Course(String cid) {
+		super();
+		System.out.println("1-args Constructor");
+		this.cid = cid;
+	}
+	
+	public Course(String cid, String cname) {
+		super();
+		System.out.println("2-args Constructor");
+		this.cid = cid;
+		this.cname = cname;
+	}
+
 	
 	public Course(String cid, String cname, int cost) {
 		super();
+		System.out.println("3-args Constructor");
 		this.cid = cid;
 		this.cname = cname;
 		this.cost = cost;
